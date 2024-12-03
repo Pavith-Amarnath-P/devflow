7185e10b3c028cec93f6094928f485f89ee79511
Pavith Amarnath P <greatfulpavith@gmail.com>
2024-12-03 19:53:32 +0530

    Theme + ShadCN + NavBar Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "./globals.css";
+import NavBar from "@/components/ui/navigation/navbar";
 import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
@@ -37,6 +38,7 @@ export default function RootLayout({
           defaultTheme="system"
           disableTransitionOnChange
         >
+          <NavBar />
           {children}
         </ThemeProvider>
       </body>
