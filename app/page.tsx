bc1a7fa0c22548bf180dc73a669d757ea0536a16
Pavith Amarnath P <greatfulpavith@gmail.com>
2024-12-03 19:16:25 +0530

    Tailwind CSS Setup and Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1 className="mt-2 pl-10 text-2xl font-bold text-primary-500">
+        Hello World!
+      </h1>
     </div>
   );
 }
